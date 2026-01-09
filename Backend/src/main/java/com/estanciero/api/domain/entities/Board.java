c7dfbda579f7475e9ac97a1c93c74cde317d8780
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-01-08 23:42:45 -0300

    feat(Board): add game relation
    refactor: add @ToString restriction
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

--- Backend/src/main/java/com/estanciero/api/domain/entities/Board.java
+++ Backend/src/main/java/com/estanciero/api/domain/entities/Board.java
@@ -1,6 +1,7 @@
 package com.estanciero.api.domain.entities;
 
 import com.estanciero.api.domain.entities.boxes.Box;
+import com.estanciero.api.domain.entities.boxes.Game;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -14,12 +15,19 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(onlyExplicitlyIncluded = true)
 public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private Long currentTurnPlayerId;
+
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
     @OrderBy("position ASC")
     private List<Box> boxes = new ArrayList<>();
+
+    @OneToOne()
+    @JoinColumn(name = "game_id")
+    private Game game;
 }
