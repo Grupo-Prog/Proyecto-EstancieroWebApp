7fd85ed7487df839826da6cc741897b8ebfd94e1
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-01-11 01:56:58 -0300

    logs: add log for db connection
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

--- Backend/src/main/java/com/estanciero/api/BackendApplication.java
+++ Backend/src/main/java/com/estanciero/api/BackendApplication.java
@@ -1,7 +1,12 @@
 package com.estanciero.api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,4 +15,19 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner probarConexion(DataSource dataSource) {
+        return args -> {
+            try (Connection conn = dataSource.getConnection()) {
+                System.out.println("----------------------------------------");
+                System.out.println("¡CONEXIÓN EXITOSA!");
+                System.out.println("Conectado a la base de datos: " + conn.getCatalog());
+                System.out.println("----------------------------------------");
+            } catch (Exception e) {
+                System.err.println("----------------------------------------");
+                System.err.println("ERROR DE CONEXIÓN : " + e.getMessage());
+                System.err.println("----------------------------------------");
+            }
+        };
+    }
 }
