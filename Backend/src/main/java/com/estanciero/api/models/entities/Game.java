da4de17b2dfcb7d17ad2c15ca3645ef1caecda7c
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-01-25 20:22:21 -0300

    feat(Game): add method validateJoinability
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

--- Backend/src/main/java/com/estanciero/api/models/entities/Game.java
+++ Backend/src/main/java/com/estanciero/api/models/entities/Game.java
@@ -27,6 +27,9 @@ public class Game {
 
     private Long currentTurnPlayerId;
 
+    public static final Integer MAX_PLAYERS = 6;
+
+
     @Builder.Default
     @OneToMany(mappedBy = "game", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Player> players = new ArrayList<>();
@@ -36,4 +39,27 @@ public class Game {
 
     @Enumerated(EnumType.STRING)
     private GameStatusType statusType;
+
+
+    /**
+     * Metodo creado para que la entidad
+     * verifique por sí misma si se cumplen las condiciones para unirse o no
+     *
+     * @param userId Usuario que intenta unirse
+     */
+    public void validateJoinability(Long userId) {
+        //chequear que no haya comenzado
+        if (this.statusType != GameStatusType.LOBBY) {
+            throw new IllegalArgumentException("The game is not in lobby");
+        }
+        //chequear que tenga lugar
+        if (this.players.size() >= MAX_PLAYERS) {
+            throw new IllegalStateException("The game is full");
+        }
+        //chequear que el usuario no ingresó todavía
+        boolean alreadyJoined = this.players.stream().anyMatch(p -> p.getId().equals(userId));
+        if (alreadyJoined) {
+            throw new IllegalStateException("The user is already in this lobby");
+        }
+    }
 }
