4b8a32650517e08964dc7fb72e339e5497f0799e
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-01-25 22:43:06 -0300

    feat: add CreateGameDTO
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

---
+++ Backend/src/main/java/com/estanciero/api/dtos/game/CreateGameDTO.java
@@ -0,0 +1,11 @@
+package com.estanciero.api.dtos.game;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record CreateGameDTO(
+        @NotNull(message = "The user ID is required")
+        @Positive(message = "The user ID must be positive")
+        Long hostUserId
+) {
+}
