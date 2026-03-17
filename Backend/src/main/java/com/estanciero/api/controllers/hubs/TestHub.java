b72469dfeb4156ec7f3c1a903ff1ae474712c72f
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-03-17 17:16:58 -0300

    feat: add TestHub

---
+++ Backend/src/main/java/com/estanciero/api/controllers/hubs/TestHub.java
@@ -0,0 +1,28 @@
+package com.estanciero.api.controllers.hubs;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import java.util.Map;
+
+@Controller
+public class TestHub {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public TestHub(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping("/test")
+    public void testWebsocket(@Payload Map<String, String> payload) {
+        var mensaje = payload.get("content");
+        System.out.println("=========================");
+        System.out.println("MENSAJE RECIBIDO:" + mensaje);
+        System.out.println("=========================");
+
+        Map<String, String> respuesta = Map.of("content", "Mandaste: " + mensaje);
+        messagingTemplate.convertAndSend("/topic/test", respuesta);
+    }
+}
