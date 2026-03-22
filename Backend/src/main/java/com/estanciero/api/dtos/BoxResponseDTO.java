fb4c9885c39fb4389f639465f946f4956dbe7c00
412285-Cotaimich <412285@tecnicatura.frc.utn.edu.ar>
2026-03-22 02:29:39 -0300

    muerte a las notebooks

--- Backend/src/main/java/com/estanciero/api/dtos/BoxResponseDTO.java
+++ Backend/src/main/java/com/estanciero/api/dtos/BoxResponseDTO.java
@@ -1,5 +1,6 @@
 package com.estanciero.api.dtos;
 
+import com.estanciero.api.models.entities.Player;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,4 +17,12 @@ public class BoxResponseDTO {
     private String name;
     private Integer position;
     private String type;
+
+    private Double purchasePrice;
+    private Integer chacrasCount;
+    private Boolean hasEstancia;
+    private Boolean isMortgage;
+
+    private PlayerResponseDTO owner;
+
 }
--- Backend/src/main/java/com/estanciero/api/mappers/BoxMapper.java
+++ Backend/src/main/java/com/estanciero/api/mappers/BoxMapper.java
@@ -2,14 +2,30 @@ package com.estanciero.api.mappers;
 
 import com.estanciero.api.dtos.BoxResponseDTO;
 import com.estanciero.api.models.entities.boxes.Box;
+import com.estanciero.api.models.entities.boxes.PropertyBox;
 
 public class BoxMapper {
 
     public static BoxResponseDTO toDTO(Box box) {
-        return BoxResponseDTO.builder()
-                .position(box.getPosition())
+
+        BoxResponseDTO.BoxResponseDTOBuilder dto = BoxResponseDTO.builder()
                 .name(box.getName())
-                .type(box.getClass().getSimpleName())
-                .build();
+                .position(box.getPosition())
+                .type(box.getClass().getSimpleName());
+
+        if (box instanceof PropertyBox propertyBox) {
+            dto.purchasePrice(propertyBox.getPurchasePrice());
+            dto.chacrasCount(propertyBox.getChacrasCount());
+            dto.hasEstancia(propertyBox.getHasEstancia());
+            dto.isMortgage(propertyBox.getIsMortgage());
+
+            dto.owner(
+                    propertyBox.getOwner() != null
+                            ? PlayerMapper.toDTO(propertyBox.getOwner())
+                            : null
+            );
+        }
+
+        return dto.build();
     }
 }
